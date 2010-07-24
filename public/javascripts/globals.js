f4c2b702059f33d9f51e755db4e46c07d815b70c
Ashleigh Baumgardner <lash@boob.local>
2010-07-24 01:13:19 -0700

    Added blogger.js and modified it to add a class with out user names. Then slightly different css for our usernames to distinguish tweets.

---
+++ public/javascripts/globals.js
@@ -0,0 +1,66 @@
+var gLocalSearch;
+var gMap;
+var gInfoWindow;
+
+jQuery(function() {
+  // Initialize the map with default UI.
+  gMap = new google.maps.Map(document.getElementById("map_canvas"), {
+    center: new google.maps.LatLng(47.6543, -122.2634),
+    zoom: 10,
+    mapTypeId: 'roadmap'
+  });
+
+  // Create some fancy waypoint thingies
+  var track_points = jQuery(waypoints).map(function(i, p) { return new google.maps.LatLng(p[0], p[1]) }).get();
+  var polyline = new google.maps.Polyline({
+        path: track_points,
+        strokeColor: "#FF6600",
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+      });
+  polyline.setMap(gMap);
+
+  // Toggle full screen
+  jQuery("#map_full_screen").click( function() {
+    jQuery("#map_canvas").toggleClass("full_screen");
+  });
+
+  // Create one InfoWindow to open when a marker is clicked.
+  gInfoWindow = new google.maps.InfoWindow;
+  google.maps.event.addListener(gInfoWindow, 'closeclick', function() {
+    unselectMarkers();
+  });
+
+  // Initialize the local searcher
+  gLocalSearch = new GlocalSearch();
+  gLocalSearch.setSearchCompleteCallback(null, OnLocalSearch);
+
+  // dosearch is the div id of the button
+  jQuery("#queryInput").change(doSearch);
+  jQuery("#dosearch").click(doSearch);
+  jQuery(".addSuggestionFromSearch").live("click", createSuggestionFromSearch);
+
+  jQuery(".icon_link").live("click", function(event) {
+      var me = this;
+      jQuery(".icon_link").removeClass("select");
+      select(me); 
+  });
+});
+
+function select(me) {
+  jQuery("#suggestion_icon_id").val(me.title);
+  me.addClassName("select");
+}
+
+function createSuggestionFromSearch(event) {
+  var parentdiv = jQuery(this).parents(".unselected")
+  var suggestionLatLng = parentdiv.find(".hiddenLatLng").text().split(', ');
+  var suggestedName = parentdiv.find("a.gs-title").text();
+  jQuery.get("/map/new_suggestion",
+             { lat: suggestionLatLng[0],
+               lng: suggestionLatLng[1],
+               title: suggestedName },
+             function(stuff) {
+               jQuery.fancybox({ content: stuff, scrolling: "no" });
+             });
+}
