12751bd0c8fd2af25be89a3257eaf243910c227f
Ashleigh Baumgardner <lash@boob.local>
2010-07-30 14:11:10 -0700

    Fixed lat/lon for saving a search.

--- public/javascripts/globals.js
+++ public/javascripts/globals.js
@@ -69,7 +69,7 @@ function select(me) {
 
 function createSuggestionFromSearch(event) {
   var parentdiv = jQuery(this).parents(".unselected")
-  var suggestionLatLng = parentdiv.find(".hiddenLatLng").text().split(', ');
+  var suggestionLatLng = $(".hiddenLatLng").text().split(', ');
   console.log(suggestionLatLng);
   var suggestedName = parentdiv.find("a.gs-title").text();
   jQuery.get("/map/new_suggestion",
