af26f94d36802f1d0189fefdcf8df0f9fb35e2d2
Ashleigh Baumgardner <lash@boob.local>
2010-08-07 22:52:23 -0700

    Changed to user model to allow blank and nil email and name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   has_many :suggestions
   has_many :posts
   validates_uniqueness_of :openid
-  validates_uniqueness_of :name
-  validates_uniqueness_of :email
+  validates_uniqueness_of :name, :allow_nil => true, :allow_blank => true
+  validates_uniqueness_of :email, :allow_nil => true, :allow_blank => true
   def nickname
     (self.name.nil? or self.name == "") ? "Guest_#{id}" : self.name
   end
