734b8acabadcf81d6b398fcd2fa10815ff88f77f
Benson Kalahar <bensonk42@gmail.com>
2010-09-15 17:34:25 -0700

    Added comment edit & destroy functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,46 @@
 class CommentsController < ApplicationController
   before_filter :must_be_user
-  before_filter :must_own_comment, :only => [ :update, :destroy ]
-  def post
-    post = Post.find params[:id]
-    comment = Comment.new
-    comment.position = post
-    comment.body = params[:comment][:body]
-    comment.user = @user
-    comment.save
-    redirect_to post
+  before_filter :must_own_comment, :only => [ :edit, :update, :destroy ]
+
+  def index
+    @comments = Comment.find(:all, :order => "created_at DESC")
   end
 
-  def suggestion
-    suggestion = Suggestion.find params[:id]
-    comment = Comment.new
-    comment.position = suggestion
-    comment.body = params[:comment][:body]
+  def create
+    comment = Comment.new params[:comment]
     comment.user = @user
     comment.save
-    redirect_to suggestion
+    redirect_to comment.position
+  end
+
+  def edit
   end
 
   def update
-    comment = Comment.find params[:id]
-    comment.body = params[:comment][:body]
-    comment.save
+    @comment.body = params[:comment][:body]
+    flash[:notice] = "The comment was updated"
+    @comment.save
+    redirect_to @comment.position
+  end
+
+  def destroy
+    @comment.destroy
+    flash[:notice] = "Comment was removed"
+    redirect_to @comment.position
+  end
+
+  private
+  def must_own_comment
+    unless params[:id]
+      flash[:error] = "You must specify a comment!"
+      redirect_to :controller => :home
+      return false
+    end
+    @comment = Comment.find params[:id]
+    unless @user and (@user.admin? or @user == @comment.user)
+      flash[:error] = "You must own the comment you wish to modify."
+      redirect_to :controller => :home
+      return false
+    end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,19 @@
 class Comment < ActiveRecord::Base
+  validates_presence_of :position
   belongs_to :position, :polymorphic => :true
   belongs_to :user
 
   def author
     if user
       user.name
+    else
+      "unknown"
+    end
+  end
+
+  def position_name
+    if position
+      position.title
     else
       "unknown"
     end
