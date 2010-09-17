b3005fd1abd04710ce726f3bb04795fc1269ad4e
Benson Kalahar <bensonk42@gmail.com>
2010-09-16 17:31:21 -0700

    Tweaked default number of posts on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @photos = FlickrPhoto.find :all, :order => "id DESC", :limit => 5
-    @posts = Post.find :all, :order => "created_at DESC", :limit => 3
+    @posts = Post.find :all, :order => "created_at DESC", :limit => 4
   end
 end
