0fcb743f16f042e08891ffda1d11bb7fd8d1f82d
Benson Kalahar <bensonk42@gmail.com>
2011-03-17 02:20:13 +0000

    Fixed environment reference

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,6 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+SECRET = ENV['CLOUDMAILIN_SECRET']
+
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
